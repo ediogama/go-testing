04a22908591a80f6f8028976abe8f289abdf67ad
Edio Gama <ediogama@hotmail.com>
2023-01-23 22:00:20 -0300

    add test fuzz

--- 1/tax_test.go
+++ 1/tax_test.go
@@ -48,3 +48,21 @@ func BenchmarkCalculateTax2(b *testing.B) {
 		CalculateTax2(500.0)
 	}
 }
+
+// run go test -fuzz=. -run=^#
+
+func FuzzCalculateTax(f *testing.F) {
+	seed := []float64{-1, -2, -2.5, 500.0, 1000.0, 1501.0}
+	for _, amount := range seed {
+		f.Add(amount)
+	}
+	f.Fuzz(func(t *testing.T, amount float64) {
+		result := CalculateTax(amount)
+		if amount <= 0 && result != 0 {
+			t.Errorf("Reseived %f but expected 0", result)
+		}
+		if amount > 20000 && result != 20 {
+			t.Errorf("Reseived %f but expected 20", result)
+		}
+	})
+}
